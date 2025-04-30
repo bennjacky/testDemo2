cacbb078215d6fda916558dcb9315ada5328277c
BEN <bennjacky0936@gmail.com>
2025-04-30 16:57:53 +0800

    加入資料庫初始空預防

--- Demo2/Demo2/Services/ProductService.cs
+++ Demo2/Demo2/Services/ProductService.cs
@@ -12,7 +12,13 @@ namespace Demo2.Services
 
         private readonly ProductRepository _repo = new ProductRepository();
 
-        public IEnumerable<Product> GetAll() => _repo.GetAll();
+        //public IEnumerable<Product> GetAll() => _repo.GetAll();
+        public IEnumerable<Product> GetAll()
+        {
+            var list = _repo.GetAll();
+            return list ?? new List<Product>(); // 避免傳回 null
+        }
+
         public void Add(Product product) => _repo.Add(product);
         public Product GetById(int id) => _repo.GetById(id);
         public void Update(Product product) => _repo.Update(product);
